a00271736c090bb307df1f398f41e7ade889aaa5
Gouvernour <larssonfabian0@gmail.com>
2023-02-04 19:24:48 +0100

    Update Tool.cs

--- Assets/Scripts/Tool.cs
+++ Assets/Scripts/Tool.cs
@@ -7,13 +7,13 @@ public enum ToolType
 {
     Axe,
     WaterGun,
-    BugSpray
+    BugSpray,
+    Fertilizer
 }
 public class Tool : MonoBehaviour
 {
     CapsuleCollider2D col;
-    
-    Vector3 throwVector = Vector3.left;
+    Vector3 rotationSpeed = new Vector3(0, 0, 3);
     public Transform _parent;
     public float raycastDistance;
     bool thrown = false;
@@ -27,8 +27,8 @@ public class Tool : MonoBehaviour
         if(col == null)
         {
             col = gameObject.AddComponent<CapsuleCollider2D>();
-            col.isTrigger = true;
         }
+        col.isTrigger = true;
     }
 
     public void Update()
@@ -49,7 +49,7 @@ public class Tool : MonoBehaviour
         {
             case ToolType.Axe:
                 //Melee
-                hits = Physics2D.RaycastAll(origin: transform.position, direction, raycastDistance);
+                hits = Physics2D.RaycastAll(origin: transform.position, direction, raycastDistance * 3);
                 //If hit => Do Damage
                 foreach (RaycastHit2D hit in hits)
                 {
@@ -150,7 +150,7 @@ public class Tool : MonoBehaviour
             hits = Physics2D.RaycastAll(transform.position, direction, raycastDistance);
             foreach (RaycastHit2D h in hits)
             {
-                if (h && h.collider.gameObject.transform != _parent && h.collider.gameObject != gameObject)
+                if (h.collider.gameObject.transform != _parent && h.collider.gameObject != gameObject)
                 {
                     thrown = false;
                     //Do damage to hit player
@@ -162,7 +162,7 @@ public class Tool : MonoBehaviour
                 }
                 else
                 {
-                    transform.Rotate(new Vector3(0, 0, 3));
+                    transform.Rotate(rotationSpeed);
                     transform.position += direction * Time.deltaTime * 10;
                 }
             }
